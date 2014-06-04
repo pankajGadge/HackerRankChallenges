6e7c5db73720183d3c3829b94b92e0a7dbb185f3
pankajGadge <pankajg@devero.com>
2014-06-03 23:54:51 -0700

    Adding a method that finds the changes for anagram and java docs

--- src/com/hackerrank/StringAnagram.java
+++ src/com/hackerrank/StringAnagram.java
@@ -1,10 +1,25 @@
 package com.hackerrank;
 
+/**
+ * The problem definition can be found here, 
+ * https://www.hackerrank.com/challenges/anagram
+ * 
+ * @author pankajg
+ * gadgepankaj4@gmail.com
+ */
 public class StringAnagram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	/**
+	 * This method finds the changes required in order to 
+	 * find the anagram for the given string
+	 * @param test The given string
+	 * @return
+	 */
+	public static int findAnagramChanges(String test) {
+		return 0;
+	}
 }
